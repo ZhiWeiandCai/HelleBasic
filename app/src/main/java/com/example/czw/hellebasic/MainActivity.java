ded15ed8a35903f769bfec47173edf8e8dca2efe
czw <543481852@qq.com>
2017-06-19 10:35:06 +0800

    commit 2017-6-19

--- app/src/main/java/com/example/czw/hellebasic/MainActivity.java
+++ app/src/main/java/com/example/czw/hellebasic/MainActivity.java
@@ -1,19 +1,35 @@
 package com.example.czw.hellebasic;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            finish();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +48,67 @@ public class MainActivity extends AppCompatActivity {
             }
         });*/
 
-
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.example.czw.hellebasic.my_broadcast");
+        registerReceiver(broadcastReceiver, filter);
+        Log.e(TAG, "onCreate方法被调用");
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.e(TAG, "onActivityResult方法被调用");
+        if (101 == resultCode) {
+            if (data != null) {
+
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        Log.e(TAG, "onStart方法被调用");
+    }
+
+    @Override
+    protected void onRestart() {
+        // TODO Auto-generated method stub
+        super.onRestart();
+        Log.e(TAG, "onRestart方法被调用");
+    }
+
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+        Log.e(TAG, "onResume方法被调用");
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        Log.e(TAG, "onPause方法被调用");
+    }
+
+    @Override
+    protected void onStop() {
+        // TODO Auto-generated method stub
+        super.onStop();
+        Log.e(TAG, "onStop方法被调用");
+    }
+
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+        Log.e(TAG, "onDestroy方法被调用");
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/example/czw/hellebasic/MainActivityFragment.java
+++ app/src/main/java/com/example/czw/hellebasic/MainActivityFragment.java
@@ -1,21 +1,32 @@
 package com.example.czw.hellebasic;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.czw.hellebasic.net.ApiListener;
 import com.example.czw.hellebasic.net.VolleyHelperApi;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements View.OnClickListener{
 
     private TextView mTextView;
+    private Button button1;
+    private Button button2;
+    private TextView textView1;
 
     public MainActivityFragment() {
     }
@@ -26,7 +37,14 @@ public class MainActivityFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         mTextView = (TextView) view.findViewById(R.id.tv);
 
+
+
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 
     @Override
@@ -48,4 +66,7 @@ public class MainActivityFragment extends Fragment {
             }
         });
     }
+
+
+
 }
